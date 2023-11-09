d5fc3ade5c42c7958a3177da5ee52c1a9ba7208b
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-09 19:54:04 +0900

    spring security를 사용해서 로그인에는 사용되지 않지만 spring 내부적으로 일어나는 filter 이해를 위해

---
+++ src/main/java/com/sparta/springauth/filter/LoggingFilter.java
@@ -0,0 +1,29 @@
+package com.sparta.springauth.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j(topic = "LoggingFilter") // LoggingFilter 이름으로 로그찍기
+//@Component
+@Order(1) // LoggingFilter가 인증 및인가 filter보다 먼저 오도록 순서 지정
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 전처리
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURI();
+
+
+        log.info(url);
+
+        chain.doFilter(request, response); // 다음 Filter 로 이동
+
+        // 후처리
+        log.info("비즈니스 로직 완료");
+    }
+}
